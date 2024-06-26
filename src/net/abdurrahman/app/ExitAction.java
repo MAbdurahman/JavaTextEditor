47035dc101dabe4ad4eeec5ae8967096bde34adc
MAbdurahman <mdbdrrhm@gmail.com>
2024-06-26 07:07:06 -0500

    Add ExitAction with basic structure of AbstractAction

---
+++ src/net/abdurrahman/app/ExitAction.java
@@ -0,0 +1,20 @@
+package net.abdurrahman.app;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class ExitAction extends AbstractAction {
+    /**
+     * ExitAction Constructor -
+     * @param icon - the ImageIcon
+     */
+    @SuppressWarnings("OverridableMethodCallInConstructor")
+    public ExitAction(ImageIcon icon) {
+        super("Exit", icon);
+        setEnabled(true);
+
+    }//end of the CopyAction Constructor
+
+    public void actionPerformed(ActionEvent ae) {}//end of actionPerformed Method
+
+}//end of ExitAction Class
