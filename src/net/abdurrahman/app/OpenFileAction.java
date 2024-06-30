7be0fc2402ba2154fcc7077d5d4f1065e60afbc8
MAbdurahman <mdbdrrhm@gmail.com>
2024-06-30 07:19:20 -0500

    Add some functionality to OpenFileAction.java

--- src/net/abdurrahman/app/OpenFileAction.java
+++ src/net/abdurrahman/app/OpenFileAction.java
@@ -1,7 +1,12 @@
 package net.abdurrahman.app;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * OpenFileAction Class
@@ -26,5 +31,35 @@ public class OpenFileAction extends AbstractAction {
      * @param ae the event to be processed
      */
     @Override
-    public void actionPerformed(ActionEvent ae) {}//end of actionPerformed Method
+    public void actionPerformed(ActionEvent ae) {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter fileNameExtensionFilter =
+                new FileNameExtensionFilter("Text File", "txt");
+        jFileChooser.addChoosableFileFilter(fileNameExtensionFilter);
+
+        int checkInput = jFileChooser.showOpenDialog(null);
+
+        if (checkInput == JFileChooser.APPROVE_OPTION) {
+            File openedFile = jFileChooser.getSelectedFile();
+
+            try {
+                FileReader fileReader = new FileReader(openedFile);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                String string1 = "";
+                StringBuilder string2 = new StringBuilder();
+
+                while ((string1 = bufferedReader.readLine()) != null) {
+                    string2.append(string1).append("\n");
+                }
+
+                JavaTextEditor.TEXTPANE.setText(string2.toString());
+                bufferedReader.close();
+
+            } catch (IOException fileNotFoundException) {
+                fileNotFoundException.printStackTrace();
+            }
+        }
+    }//end of actionPerformed Method
 }//end of OpenFileAction Class
